e6167453e140531c04624ba323463b2fff80dca1
Quinn Thibeault <quinn.thibeault96@gmail.com>
2019-11-27 12:15:19 -0700

    Add support for loader chaining
    In order to chain this loader with another loader like html-loader, the export syntax needs to be removed. Should not break when used as only loader.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 	Author Brad Benvenuti @bradbenvenuti
 */
 module.exports = function(content) {
+	var regex = /(module\.exports\s?=\s?|exports\.default\s?=\s?|export default )"(.*)";?/;
+	var matches = content.match(regex);
+	content = (matches && matches[2]) ? matches[2] : content;
+	
 	this.cacheable && this.cacheable();
 	this.value = content;
 	return 'module.exports=function(scope){ return `' + content + '`};';
